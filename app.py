03d093eefffeb485a29e6c55312ebb1e05f0253e
AMBATI DEVARSHINI <devarshinee.ambati@gmail.com>
2025-09-27 15:40:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
 7rtygehrhfbend
 iughfvbend sc
 ueghdbcnx
+name = "Alice"  # String
+age = 30        # Integer
+height = 1.75   # Float
+is_student = True # Boolean
+
+print(f"Name: {name}, Age: {age}, Height: {height}, Student: {is_student}")
+3. Basic Arithmetic Operations:
+Python
+
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+modulus = num1 % num2 # Remainder
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+print(f"Modulus: {modulus}")
+4. Conditional Statements (if-else):
+
